fb28670ae0e81ae5c9d3af8f350b45e7f85ee836
choideakook <shdrnrhd113@naver.com>
2023-03-06 15:18:03 +0900

    test 기반 완료

--- src/test/java/org/example/Appliction/TestUtil.java
+++ src/test/java/org/example/Appliction/TestUtil.java
@@ -1,15 +1,32 @@
 package org.example.Appliction;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import org.assertj.core.api.ByteArrayAssert;
+
+import java.io.*;
 import java.util.Scanner;
 
 public class TestUtil {
-
-
-    public static Scanner getScanner(String input) {
+    // gen == generate 생성하다.
+    public static Scanner genScanner(String input) {
         InputStream in = new ByteArrayInputStream(input.getBytes());
 
         return new Scanner(in);
     }
+
+    public static ByteArrayOutputStream setOutToByteArray() {
+        final ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+
+        return output;
+    }
+
+    public static void clearSetOutToByteArray(ByteArrayOutputStream output) {
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+        try {
+            output.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
+
